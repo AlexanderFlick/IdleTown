b519fdf036a84223cd8755d1c52e80b2db177653
Alex <macaronisupermoon@gmail.com>
2020-08-12 00:29:32 -0500

    started stone

---
+++ IsThisThingOn/BLL/Models/Stone.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Models
+{
+    public class Stone
+    {
+        public int Total { get; set; }
+        public int Max { get; set; } = 10;
+        public int Price { get; set; } = 7;
+        public int PerClick { get; set; } = 1;
+        public bool Earn { get; set; } = false;
+        public int Sold { get; set; } = 1;
+    }
+}
