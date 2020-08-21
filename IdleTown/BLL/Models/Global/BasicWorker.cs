621a414c34b1527e9af956deeabadecbd30882f3
AlexanderFlick <macaronisupermoon@gmail.com>
2020-08-21 15:53:16 -0500

    pet peeve

--- IdleTown/BLL/Models/Global/BasicWorker.cs
+++ IdleTown/BLL/Models/Global/BasicWorker.cs
@@ -3,10 +3,10 @@
     public class BasicWorker
     {
         public int Cost { get; set; } = 4;
-        public bool Active { get; set; } = false;
         public int TotalHarvest { get; set; }
         public int PerClick { get; set; } = 1;
         public int IncreaseWithPurchase { get; set; } = 10;
         public int HarvestRate { get; set; } = 1;
+        public bool Active { get; set; } = false;
     }
 }
