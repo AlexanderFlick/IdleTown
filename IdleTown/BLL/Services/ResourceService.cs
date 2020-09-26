e788fb3b7e113b9ce024a4da292eb22aa0bf58b5
AlexanderFlick <macaronisupermoon@gmail.com>
2020-09-26 17:50:11 -0500

    added base test and started resource service

---
+++ IdleTown/BLL/Services/ResourceService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.Services
+{
+    public interface IResourceService
+    {
+
+    }
+    public class ResourceService : IResourceService
+    {
+    }
+}
---
+++ IdleTown/BLLTests/ServiceTests/ResourceServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLLTests.ServiceTests
+{
+    class ResourceServiceTests
+    {
+    }
+}
